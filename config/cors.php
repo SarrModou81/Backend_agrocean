b475bc19f980ba5b42f91c2d6eb903c630345557
SarrModou81 <mdsa2r81@gmail.com>
2025-11-26 23:52:56 +0000

    initiale 1

--- config/cors.php
+++ config/cors.php
@@ -1,23 +1,14 @@
 <?php
 
 return [
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'https://frontend-agrocean-fh3j.vercel.app',
-        'http://localhost:4200',
-        'http://localhost:3000',
-    ],
-
-    'allowed_origins_patterns' => ['*'],
-
+    'allowed_origins' => ['frontend-agrocean-fh3j.vercel.app','http://localhost:4200'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
+    'supports_credentials' => false,
 
-    'supports_credentials' => true,
 ];
