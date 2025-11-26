bc1c6b9747a515d2c164fc0790a434be185c2b6f
SarrModou81 <mdsa2r81@gmail.com>
2025-11-26 15:15:24 +0000

    test connexion

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => ['frontend-agrocean-fh3j.vercel.app','http://localhost:4200'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
