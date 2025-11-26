f1932549e3969587445a0fefa404f02cc67a8cf4
SarrModou81 <mdsa2r81@gmail.com>
2025-11-26 17:39:53 +0000

    first railway connexion1

--- config/cors.php
+++ config/cors.php
@@ -2,13 +2,23 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'auth/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['frontend-agrocean-fh3j.vercel.app','http://localhost:4200','http://localhost:3000'],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://frontend-agrocean-fh3j.vercel.app',
+        'http://localhost:4200',
+        'http://localhost:3000',
+    ],
+
+    'allowed_origins_patterns' => ['*'],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
 ];
